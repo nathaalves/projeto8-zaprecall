2e26daa3ce10e86de246a8600ae15fb234bd8b09
nathalaves <nathaalvesaraujo@gmail.com>
2022-05-10 23:40:47 -0300

    conclusão layout flascard page

--- src/FlashcardPage.js
+++ src/FlashcardPage.js
@@ -1,4 +1,9 @@
+import React from "react"
+
 export default function FlashcardPage () {
+
+    const [conpleted, setComplited] = React.useState(0)
+    const [total, setTotal] = React.useState(0)
 
     return (
         <>
@@ -6,6 +11,12 @@ export default function FlashcardPage () {
                 <img src="./images/logo-pequeno.png" alt="logo"></img>
                 <h1>ZapRecall</h1>
             </header>
+            <main className="flashcard-page-main">
+
+            </main>
+            <footer className="flashcard-page-footer">
+                {conpleted}/{total} CONCLUÍDOS
+            </footer>
         </>
     )
 }
